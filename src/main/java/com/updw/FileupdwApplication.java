c117c7c795ac9f52749c2e8bb940513ddbba502c
sarah Foster <contatosarahfoster@gmail.com>
2022-05-18 15:06:25 -0300

    inicial commit

---
+++ src/main/java/com/updw/FileupdwApplication.java
@@ -0,0 +1,13 @@
+package com.updw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileupdwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileupdwApplication.class, args);
+	}
+
+}
